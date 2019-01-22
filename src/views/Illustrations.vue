3c465f0f7ef2f0d2754fb6044c85b1c142dc99b8
wangkun <wangkun@meitunmama.com>
2019-01-22 14:10:50 +0800

    init & home page

---
+++ src/views/Illustrations.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    illustrations
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Resume.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    resume
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
