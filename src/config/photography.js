7c4d440b3791fccc4fd6d4572fed07eac4a447c4
Neo <neo@matrix>
2019-02-01 02:52:21 +0000

    Added resource files.

--- src/config/photography.js
+++ src/config/photography.js
@@ -5,22 +5,46 @@ export default {
   columns: [
     //  第一列
     [
-      require('../assets/2.5D-animinal.png'),
-      require('../assets/Huoli.png'),
-      require('../assets/IMG_0058.jpg'),
-      require('../assets/basketball.png')
+      require('../assets/photography/1.jpg'),
+      require('../assets/photography/4.jpg'),
+      require('../assets/photography/7.jpg'),
+      require('../assets/photography/10.jpg'),
+      require('../assets/photography/13.jpg'),
+      require('../assets/photography/16.jpg'),
+      require('../assets/photography/19.jpg'),
+      require('../assets/photography/22.jpg'),
+      require('../assets/photography/25.jpg'),
+      require('../assets/photography/28.jpg'),
+      require('../assets/photography/31.jpg')
     ],
     //  第二列
     [
-      require('../assets/basketball.png'),
-      require('../assets/IMG_0058.jpg'),
-      require('../assets/2.5D-animinal.png')
+      require('../assets/photography/2.jpg'),
+      require('../assets/photography/5.jpg'),
+      require('../assets/photography/8.jpg'),
+      require('../assets/photography/11.jpg'),
+      require('../assets/photography/14.jpg'),
+      require('../assets/photography/17.jpg'),
+      require('../assets/photography/20.jpg'),
+      require('../assets/photography/23.jpg'),
+      require('../assets/photography/26.jpg'),
+      require('../assets/photography/29.jpg')
     ],
     //  第三列
     [
-      require('../assets/IMG_0058.jpg'),
-      require('../assets/basketball.png'),
-      require('../assets/Huoli.png')
+      require('../assets/photography/3.jpg'),
+      require('../assets/photography/6.jpg'),
+      require('../assets/photography/9.jpg'),
+      require('../assets/photography/12.jpg'),
+      require('../assets/photography/15.jpg'),
+      require('../assets/photography/18.jpg'),
+      require('../assets/photography/21.jpg'),
+      require('../assets/photography/24.jpg'),
+      require('../assets/photography/27.jpg'),
+      require('../assets/photography/30.jpg'),
+      require('../assets/photography/32.jpg'),
+      require('../assets/photography/33.jpg'),
+      require('../assets/photography/34.jpg')
     ]
   ]
 }
